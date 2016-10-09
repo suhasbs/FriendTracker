315d8e03360d6649035014407ec3123cb2213015
suhasbs <suhasbs.nitk@gmail.com>
2016-10-10 01:04:44 +0530

    Going back to the Login Page from Register Activity

--- app/src/main/java/com/example/suhas/friendtracker/RegisterActivity.java
+++ app/src/main/java/com/example/suhas/friendtracker/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.suhas.friendtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,6 +20,20 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText etPhone = (EditText)findViewById(R.id.etPhone);
         final EditText etEmail = (EditText)findViewById(R.id.etEmail);
         final Button bRegister = (Button) findViewById(R.id.bRegister);
+
+        //*****************
+        //Data entered to be sent to the cloud before going back to the Login Activity
+        //*****************
+
+        bRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent =new Intent(RegisterActivity.this, LoginActivity.class);
+                RegisterActivity.this.startActivity(registerIntent);
+
+
+            }
+        });
 
     }
 }
